cb02121dfe4af439f1ad16dec4c000a8d1605f67
Nima Bidari <nima.bidari@FVHYKPZ6J1WK.local>
2019-10-04 14:41:24 -0400

    update

--- bioDiversityCalc/biodiversityCalc.py
+++ bioDiversityCalc/biodiversityCalc.py
@@ -1,6 +1,7 @@
 import json
 
-fileName = input("Input file name: ")
+fileName = input("Input file name: ") #Dataset
+outputFileName = input("Output file name: ") #Summary Table
 
 #Loading JSON
 with open(fileName) as json_file:
@@ -8,6 +9,7 @@ with open(fileName) as json_file:
 
 #Var
 outputData = {}
+commonNames = []
 numberOfSpecies = 0
 
 
@@ -16,11 +18,14 @@ for i in range(0,len(data)):
 		if data[i]['Scientific'] == data[i-1]['Scientific']: #Listing number of each species
 			outputData[data[i]['Scientific']] += 1 #Counting number of species
 		else:
-			outputData[data[i]['Scientific']] = 1
+			outputData[data[i]['Scientific']] = 1 #Starting new species
+			commonNames.append(data[i]['Common'])
+
 			numberOfSpecies += 1
 	else: #Edge Case
 		outputData[data[i]['Scientific']] = 1 #Starting the algorithm
 		numberOfSpecies += 1
+		commonNames.append(data[i]['Common'])
 
 #Simpson's reciprocal diversity Index
 speciesIndividualVal = list(outputData.values())
@@ -38,8 +43,34 @@ print(num)
 print(denom)
 
 srd = num/denom
+relBioDiversity = srd/numberOfSpecies
 
 
+#Output for Simpsons reciprocal diversity index
 print(outputData)
+print("\n######################################\n")
 print("The number of different species were: " + str(numberOfSpecies))
-print("The Simpson Reciprocal Index of the dataset was " + str(srd))
+print("\nThe Simpson Reciprocal Index of the dataset was " + str(srd))
+print("\nThe relative biodiversity of " + fileName + " is " + str(round(relBioDiversity * 100, 1000)) + "%.")
+
+
+######Summary Table#########
+
+#Altering File name
+outputFileName = "SummaryDataTable-" + outputFileName + ".txt"
+
+sumTab = open(outputFileName, 'w+') #Create file
+
+dataList = list(outputData) #gets a list of all species names
+
+sumTab.write('Scientific,Common,SpeciesVal \n')
+
+for i in range(0, len(outputData)):
+	sumTab.write(dataList[i] + "," + commonNames[i] + "," + str(speciesIndividualVal[i]) + "\n") #Adds data to txt file
+#User can convert to csv by renaming
+
+
+
+
+
+
