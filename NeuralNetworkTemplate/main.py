a3e8fde1121e98e87070e727dc0210a5b19979ef
Nima Bidari <nima.bidari@[nima.bidari]>
2019-11-06 15:03:26 -0500

    Neural network template

---
+++ NeuralNetworkTemplate/main.py
@@ -0,0 +1,6 @@
+import json
+import numpy as np
+from train_neural_network import NeuralNetwork
+
+
+NeuralNetwork()
---
+++ NeuralNetworkTemplate/train_neural_network.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+class NeuralNetwork():
+
+	def __init__(self):
+     pass
