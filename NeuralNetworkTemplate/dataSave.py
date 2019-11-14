e825c3311b15e42bad543e81061f478e59fe36b7
Nima Bidari <nima.bidari@[nima.bidari]>
2019-11-14 16:34:51 -0500

    Ibrahim is the best

--- NeuralNetworkTemplate/dataSave.py
+++ NeuralNetworkTemplate/dataSave.py
@@ -3,4 +3,16 @@ import json
 class DataSave():
     
     def __init__(self):
-		
+        self.outputData = {}
+
+    def read(self, fileName):
+        print("TEST")
+
+    def write(self, input):
+        print("TEST")
+
+
+
+#Troubleshooting
+if __name__ == "__main__":
+    dataSave = DataSave()
